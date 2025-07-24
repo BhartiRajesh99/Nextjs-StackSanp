2eb721420993effe554cc21d09f34684f81691b7
RAJ <rajeshbharti1110@gmail.com>
2025-07-24 17:52:45 +0530

    fix landing page

--- src/app/components/LatestQuestions.tsx
+++ src/app/components/LatestQuestions.tsx
@@ -1,5 +1,4 @@
-export const dynamic = "force-dynamic";
-
+"use client";
 import QuestionCard from "@/components/QuestionCard";
 import {
   answerCollection,
@@ -9,45 +8,52 @@ import {
 } from "@/models/name";
 import { databases, users } from "@/models/server/config";
 import { UserPrefs } from "@/store/Auth";
-import { Query } from "node-appwrite";
+import { Models, Query } from "node-appwrite";
+import React from "react";
 
-export const LatestQuestions = async () => {
-  const questions = await databases.listDocuments(db, questionCollection, [
-    Query.limit(5),
-    Query.orderDesc("$createdAt"),
-  ]);
-
-  questions.documents = await Promise.all(
-    questions.documents.map(async (ques) => {
-      const [author, answers, votes] = await Promise.all([
-        users.get<UserPrefs>(ques.authorId),
-        databases.listDocuments(db, answerCollection, [
-          Query.equal("questionId", ques.$id),
-          Query.limit(1),
-        ]),
-        databases.listDocuments(db, voteCollection, [
-          Query.equal("type", "question"),
-          Query.equal("typeId", ques.$id),
-          Query.limit(1),
-        ]),
+export const LatestQuestions = () => {
+  const [ques, setQues] = React.useState<Models.Document[]>([]);
+  React.useEffect(() => {
+    (async () => {
+      const questions = await databases.listDocuments(db, questionCollection, [
+        Query.limit(5),
+        Query.orderDesc("$createdAt"),
       ]);
 
-      return {
-        ...ques,
-        totalAnswers: answers.total,
-        totalVotes: votes.total,
-        author: {
-          $id: author.$id,
-          reputation: author.prefs.reputation,
-          name: author.name,
-        },
-      };
-    })
-  );
+      questions.documents = await Promise.all(
+        questions.documents.map(async (ques) => {
+          const [author, answers, votes] = await Promise.all([
+            users.get<UserPrefs>(ques.authorId),
+            databases.listDocuments(db, answerCollection, [
+              Query.equal("questionId", ques.$id),
+              Query.limit(1),
+            ]),
+            databases.listDocuments(db, voteCollection, [
+              Query.equal("type", "question"),
+              Query.equal("typeId", ques.$id),
+              Query.limit(1),
+            ]),
+          ]);
+
+          return {
+            ...ques,
+            totalAnswers: answers.total,
+            totalVotes: votes.total,
+            author: {
+              $id: author.$id,
+              reputation: author.prefs.reputation,
+              name: author.name,
+            },
+          };
+        })
+      );
+      setQues(questions.documents);
+    })();
+  }, []);
 
   return (
     <div className="space-y-6">
-      {questions.documents.map((question) => (
+      {ques.map((question: Models.Document) => (
         <QuestionCard key={question.$id} ques={question} />
       ))}
     </div>
